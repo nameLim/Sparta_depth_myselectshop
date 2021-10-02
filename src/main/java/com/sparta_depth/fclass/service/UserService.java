2482aaeed010e1f30454c4ec77f195b620d0b3fd
namelim <lim49435150@gmail.com>
2021-10-02 09:28:07 +0900

    [완료] JWT까지 2주차완료

--- src/main/java/com/sparta_depth/fclass/service/UserService.java
+++ src/main/java/com/sparta_depth/fclass/service/UserService.java
@@ -27,7 +27,7 @@ public class UserService {
 
     public void registerUser(SignupRequestDto requestDto) {
 // 회원 ID 중복 확인
-        String username = requestDto.getUsername();git
+        String username = requestDto.getUsername();
         Optional<User> found = userRepository.findByUsername(username);
         if (found.isPresent()) {
             throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
